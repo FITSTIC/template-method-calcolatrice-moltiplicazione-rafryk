fd4501e0ff4093e58b33714b4357ea20a9b3da02
Raffaele Balzani <raffaele.balzani@alice.it>
2017-10-30 11:07:59 +0100

    completato esercizio

---
+++ esCalcolatriceTemplate/esCalcolatriceTemplate/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esCalcolatriceTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CalcolatriceStandard cst = new CalcolatriceStandard();
+            cst.Calcola(5,10);
+
+            Console.WriteLine();
+
+            CalcolatriceSomma cso = new CalcolatriceSomma();
+            cso.Calcola(5, 10);
+
+            Console.ReadLine();
+
+            
+        }
+    }
+
+    abstract class Calcolatrice:ICalcolatrice
+    {
+        protected abstract void Moltiplicazione(int n1,int n2);
+
+        public void Calcola(int n1, int n2)
+        {
+            Moltiplicazione(n1, n2);
+        }
+    }
+
+    interface ICalcolatrice
+    {
+
+        void Calcola(int n1,int n2);
+    }
+
+    class CalcolatriceStandard : Calcolatrice
+    {
+        protected override void Moltiplicazione(int n1,int n2)
+        {
+            Console.WriteLine(n1 * n2);
+        }
+    }
+
+    class CalcolatriceSomma: Calcolatrice
+    {
+        protected override void Moltiplicazione(int n1, int n2)
+        {
+            int result=0;
+
+            for(int i=0; i<n2;i++)
+            {
+                result += n1;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
